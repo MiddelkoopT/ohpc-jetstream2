0b8a5db0964f8ea325793e1e8ad8c8d62b922c3f
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2024-02-20 17:44:28 -0600

    Crash condition for WW4

--- openstack.tf
+++ openstack.tf
@@ -176,7 +176,7 @@ resource "openstack_networking_floatingip_associate_v2" "ohpc" {
 resource "openstack_compute_instance_v2" "ohpc" {
   name = "head"
   image_name = "Featured-RockyLinux9"
-  flavor_name = "m3.tiny"
+  flavor_name = "m3.small"
   key_pair = "orange"
   network {
     port = openstack_networking_port_v2.ohpc-external.id
