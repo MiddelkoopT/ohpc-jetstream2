58f25d161152b3e72991cc9ec1436570942400c2
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2024-08-07 11:46:16 -0500

    Fix login.sh for setting credentials

--- login.sh
+++ login.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CLOUD_CONFIG=${1:-cloud.yaml}
-CLOUD_AUTH=${1:-cloud/jetstream.env}
+CLOUD_CONFIG=${1:-clouds.yaml}
+CLOUD_AUTH=${2:-cloud/jetstream.env}
 
 echo "=== login.sh openstack ${CLOUD_CONFIG} ${CLOUD_AUTH}"
 
 tee <<EOF | pass insert -m ${CLOUD_AUTH} 
-OS_AUTH_URL=https://js2.jetstream-cloud.org:5000/v3/
+OS_AUTH_URL=$(yq .clouds.openstack.auth.auth_url < ${CLOUD_CONFIG})
 OS_AUTH_TYPE=v3applicationcredential
 OS_APPLICATION_CREDENTIAL_ID=$(yq .clouds.openstack.auth.application_credential_id < ${CLOUD_CONFIG})
 OS_APPLICATION_CREDENTIAL_SECRET=$(yq .clouds.openstack.auth.application_credential_secret < ${CLOUD_CONFIG})
