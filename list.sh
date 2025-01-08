0817aa2bccbe16d4cf337f8efdbf322923fffc9b
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-01-08 10:22:34 -0600

    Add images to list.sh

--- list.sh
+++ list.sh
@@ -6,6 +6,8 @@ openstack project list
 openstack network list
 openstack subnet list
 openstack security group list
+openstack image list --private
+openstack image list --shared
 openstack server list
 openstack floating ip list
 
