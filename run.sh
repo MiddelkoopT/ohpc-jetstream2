40486f32fb94ce9c0b49c5c174a5a39b55c31fe4
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-02-08 10:47:42 -0600

    dnf clean bug gone

--- run.sh
+++ run.sh
@@ -9,9 +9,6 @@ while ! ssh rocky@$OHPC_DNS hostname ; do echo . ; sleep .2 ; done
 
 ansible --verbose all -m ping
 
-# FIXME: strange bugfix - not sure why this works but prevents dnf update failure
-ssh rocky@$OHPC_DNS sudo dnf clean all
-
 ansible-playbook -v playbooks/system-rocky.yaml
 ansible-playbook -v playbooks/ohpc-head.yaml
 ansible-playbook -v playbooks/nodes.yaml
