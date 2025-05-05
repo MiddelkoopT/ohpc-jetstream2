9ac27b51c6d8bb52e2f99aa2d82c88640eebb685
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2025-05-05 17:22:42 -0500

    Shortcut for auth-env.sh

--- auth-env.sh
+++ auth-env.sh
@@ -2,5 +2,5 @@
 ## Source secure envrionment variables from pass
 CLOUD_AUTH=${1:-cloud/jetstream.env}
 echo "=== openstack auth-env.sh ${CLOUD_AUTH}"
-source <(pass ${CLOUD_AUTH})
+source <(pass cloud/${CLOUD_AUTH}.env)
 export OS_NAME OS_AUTH_URL OS_AUTH_TYPE OS_APPLICATION_CREDENTIAL_ID OS_APPLICATION_CREDENTIAL_SECRET
